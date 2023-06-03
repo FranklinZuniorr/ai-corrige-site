a13387a03e722fedc25d2bef4e71cc1e17c099d4
Franklin Vieira <franklinbarbs763@gmail.com>
2023-06-03 14:16:43 -0300

    Att.

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Button, Grid, Image, Segment } from "semantic-ui-react";
+import { Button, Grid, Header, Icon, Image, Segment } from "semantic-ui-react";
 import store from "../store";
 import suportLogoUser from "../img/suporte-user.png";
 
@@ -15,8 +15,16 @@ const Home = () => {
     return(
         <>
             <div className="top-menu">
-                <Image size="mini" avatar src={access.img || suportLogoUser} />
-                <Button floated="right" icon="sign-out"  />
+                <Header 
+                as="h3" 
+                floated="left" 
+                className="margin-top-mini" 
+                textAlign="left" 
+                image={access.img || suportLogoUser} 
+                content={access.username} 
+                subheader={`Coins: ${access.coins}`} 
+                />
+                <Button color="black" floated="right" icon="sign-out"  />
             </div>
         </>
     );
